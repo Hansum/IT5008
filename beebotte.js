c0887b95ec220626eaffed6ce51c7c00db977ee0
Christian Donne Vargan <christian.vargan@forwardbpo.com>
2019-05-03 15:03:40 +0800

    graph

--- beebotte.js
+++ beebotte.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express();
 var path = require("path");
 const io = require('socket.io')();
+var http = require('http');
+var bodyParser = require('body-parser')
+var data;
 // var http = require('http').createServer(app);
 const hostname = '127.0.0.1';
 const port = 8080;
@@ -11,43 +14,54 @@ var transport = {
     apiKey: 'gsMtD4jA0aKVC8pnBbpouiBb',
     secretKey: 'GM0aSlrFlw1fP3FY65qziQeQ5pTPTn2T'
 }
-// var client = new bbt.Connector({apiKey: 'gsMtD4jA0aKVC8pnBbpouiBb', secretKey: 'GM0aSlrFlw1fP3FY65qziQeQ5pTPTn2T'});
 
-// client.read({
-//     channel: '5008MQ7',
-//     resource: 'co', 
-//     limit: 5/* Retrieves last 5 records */
-//   }, function(err, res) {
-//     if (err) {
-//         throw (err)
-//     } else {
-//         console.log("heheh",res)
-//     }
-//   });
+// client = new bbt.Stream({ transport: transport });
 
-client = new bbt.Stream({ transport: transport });
+// client.on('connected', function() {
+//     client.subscribe( '5008MQ7', 'co', {read: true, write: true}, function(err,message){
+//         // res.render(__dirname + "/view/main", {msg:message});
+//         if (err) {
+//             throw(err);
+//         } else {
+//             console.log("MESSAGE",message)
+//         }
+//       })
 
-client.on('connected', function(res) {
-    client.subscribe( '5008MQ7', 'co', {read: true, write: true}, function(err,message){
-        // res.render(__dirname + "/view/main", {msg:message});
-        if (err) {
-            throw(err);
-        } else {
-            console.log("Beebotte data",message);
-        }
-      })
-})
+//       .on('subscribed', function(sub) {
+//         client.read( '5008MQ7', 'co');
+//       })
+// })
 
-// app.get('/', function(req, res) {
-//     res.render(__dirname + '/view/main',{name:'asdada'});
-// });
+var client = new bbt.Connector({apiKey: 'gsMtD4jA0aKVC8pnBbpouiBb', secretKey: 'GM0aSlrFlw1fP3FY65qziQeQ5pTPTn2T'});
+
+client.read({
+    channel: '5008MQ7',
+    resource: 'co'
+    // limit: 100/* Retrieves last 5 records */
+  }, function(err, res) {
+    if (err) {
+        throw (err)
+    } else {
+        data = res;
+        console.log("HALA KA",data)
+    }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+
+app.get('/', function(req, res) {
+    res.render(__dirname + '/view/main',{name:data});
+});
 
 app.set('view engine','ejs');
 app.set("views", path.join(__dirname, "views"));
 
-// http.listen(port, function() {
-//     console.log('Server start at port',port);
-// });
+app.listen(port, function() {
+    console.log('Server start at port',port);
+});
 
 
 
